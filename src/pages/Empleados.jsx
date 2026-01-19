0944097d8200c8cd17277ff9550e2b27f66a177a
miguelalbaap@gmail.com <miguelalbaap@gmail.com>
2026-01-19 23:09:20 +0100

    Cambios en visualización de empleados

--- src/pages/Empleados.jsx
+++ src/pages/Empleados.jsx
@@ -34,6 +34,9 @@ export default function Empleados() {
                             <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                                 Nombre
                             </th>
+                             <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                Teléfono
+                            </th>
                             <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                                 Color Agenda
                             </th>                            
@@ -44,7 +47,12 @@ export default function Empleados() {
                             <tr key={empleado.empleadoid} className="hover:bg-gray-50">
                                 <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                                     <p className="text-gray-900 whitespace-no-wrap">
-                                        {empleado.nombre} 
+                                        {empleado.nombre} {empleado.apellidos}
+                                    </p>
+                                </td>
+                                 <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <p className="text-gray-900 whitespace-no-wrap">
+                                        {empleado.telefono}
                                     </p>
                                 </td>
                                 <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
